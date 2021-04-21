6921765e65dd7f5272b5fc332d9ab8f06e8ce39a
Evens <feix0033@easv365.dk>
2021-04-21 13:52:47 +0200

    add the rentbookview in the project

---
+++ src/com/rant/RentedBooksView.java
@@ -0,0 +1,34 @@
+package com.rant;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import library.model.Book;
+import library.model.User;
+
+public class RentedBooksView {
+    public static Pane getPane(User user) {
+        Pane root = new Pane();
+
+        TableView<Book> viewBooks = new TableView<>(user.getRentedBooks());
+        viewBooks.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        viewBooks.prefWidthProperty().bind(root.widthProperty());
+        viewBooks.prefHeightProperty().bind(root.heightProperty());
+
+        TableColumn<Book, String> columnId = new TableColumn<>("ID");
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Book, String> columnTitle = new TableColumn<>("Title");
+        columnTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        TableColumn<Book, String> columnAuthor = new TableColumn<>("Author");
+        columnAuthor.setCellValueFactory(new PropertyValueFactory<>("author"));
+
+        viewBooks.getColumns().addAll(columnId, columnTitle, columnAuthor);
+
+        root.getChildren().add(viewBooks);
+
+        return root;
+    }
+}
